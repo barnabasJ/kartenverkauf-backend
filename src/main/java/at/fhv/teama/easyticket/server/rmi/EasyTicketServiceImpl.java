801400849a56000271a4f3409b504f6a3c84648d
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-10-22 20:45:10 +0200

    works

--- src/main/java/at/fhv/teama/easyticket/server/rmi/EasyTicketServiceImpl.java
+++ src/main/java/at/fhv/teama/easyticket/server/rmi/EasyTicketServiceImpl.java
@@ -4,10 +4,13 @@ import at.fhv.teama.easyticket.server.ProgramService;
 import at.fhv.teama.kartenverkauf.dto.AddressDto;
 import at.fhv.teama.kartenverkauf.dto.ProgramDto;
 import at.fhv.teama.kartenverkauf.rmi.EasyTicketService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.rmi.RemoteException;
 
+@Slf4j
 @Component
 public class EasyTicketServiceImpl implements EasyTicketService {
     private ProgramService programService;
@@ -23,7 +26,10 @@ public class EasyTicketServiceImpl implements EasyTicketService {
 
     @Override
     public void saveProgram(ProgramDto programDto) {
+        // call secured method
+        System.out.println(SecurityContextHolder.getContext().getAuthentication());
+        log.info("remote method was called");
+        programService.getAllPrograms();
         programService.saveProgram(programDto);
-
     }
 }
