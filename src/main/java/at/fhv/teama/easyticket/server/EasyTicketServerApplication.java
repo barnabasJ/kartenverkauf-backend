80537d27870a916323fe38186909e5a3a3b77ecb
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-10-23 12:29:24 +0200

    refactor: set security policy programatically

--- src/main/java/at/fhv/teama/easyticket/server/EasyTicketServerApplication.java
+++ src/main/java/at/fhv/teama/easyticket/server/EasyTicketServerApplication.java
@@ -3,11 +3,25 @@ package at.fhv.teama.easyticket.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.*;
+
 @SpringBootApplication
 public class EasyTicketServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasyTicketServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    Policy.setPolicy(
+        new Policy() {
+          @Override
+          public PermissionCollection getPermissions(CodeSource codesource) {
+            Permissions p = new Permissions();
+            p.add(new AllPermission());
+            return p;
+          }
+        });
+    if (System.getSecurityManager() == null) {
+      System.setSecurityManager(new SecurityManager());
+    }
+    System.setSecurityManager(new SecurityManager());
+    SpringApplication.run(EasyTicketServerApplication.class, args);
+  }
 }
