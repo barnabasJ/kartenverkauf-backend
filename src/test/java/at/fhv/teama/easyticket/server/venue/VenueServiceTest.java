de551af5a84c03daf08130576767936a1b9acb32
patrickzgubic <p.zgubic@hotmail.com>
2019-10-29 15:11:48 +0100

    Changed venueTest  service->controller

--- src/test/java/at/fhv/teama/easyticket/server/venue/VenueServiceTest.java
+++ src/test/java/at/fhv/teama/easyticket/server/venue/VenueServiceTest.java
@@ -21,53 +21,35 @@ public class VenueServiceTest {
   @Autowired private VenueController venueController;
   @Autowired private VenueMapper venueMapper;
 
-  @Test
-  public void getAllVenues() {
 
-    Venue oldVenue = new Venue();
-    oldVenue.setDate(LocalDateTime.now().minusDays(1));
-    Venue futureVenue = new Venue();
-    futureVenue.setDate(LocalDateTime.now().plusDays(1));
-    VenueDto futureVenueDTO = venueMapper.venueToVenueDto(futureVenue);
-    venueRepo.save(oldVenue);
-    venueRepo.save(futureVenue);
-    futureVenueDTO.setId((long) 2);
-    assertEquals(Set.of(futureVenueDTO), venueController.getAllVenues());
-    venueRepo.deleteAll();
+  private Program futureProgram;
+  private Program oldProgram;
+
+  @Before
+  public void setup() {
+
+    /*
+    futureProgram = new Program();
+    futureProgram.setDescription("Hell Cat");
+    futureProgram.setGenre("Rock");
+
+    oldProgram = new Program();
+    oldProgram.setDescription("Mama Mia");
+    oldProgram.setGenre("Theater");*/
   }
 
   @Test
-  public void getAllVenuesByGenre(){
-    Program oldProgram = new Program();
-    oldProgram.setGenre("Theater");
-    Venue oldVenue = new Venue();
+  public void getAllVenues() {
+    Venue oldVenue;
+    oldVenue = new Venue();
     oldVenue.setDate(LocalDateTime.now().minusDays(1));
-    oldVenue.setProgram(oldProgram);
-    venueRepo.save(oldVenue);
-
-    Program futureProgram0 = new Program();
-    futureProgram0.setGenre("Klassik");
-    Venue futureVenue0 = new Venue();
-    futureVenue0.setDate(LocalDateTime.now());
-    futureVenue0.setProgram(futureProgram0);
-    venueRepo.save(futureVenue0);
-
-    Program futureProgram1 = new Program();
-    futureProgram1.setGenre("Konzert");
-    Venue futureVenue1 = new Venue();
-    futureVenue1.setDate(LocalDateTime.now().plusDays(1));
-    futureVenue1.setProgram(futureProgram1);
-    venueRepo.save(futureVenue1);
-
-    Program futureProgram2 = new Program();
-    futureProgram2.setGenre("Theater");
-    Venue futureVenue2 = new Venue();
-    futureVenue2.setDate(LocalDateTime.now().plusDays(2));
-    futureVenue2.setProgram(futureProgram2);
-    venueRepo.save(futureVenue2);
-
-    VenueDto futureVenue2DTO = venueMapper.venueToVenueDto(futureVenue2);
-    assertEquals(Set.of(futureVenue2DTO),venueController.getAllVenuesByGenre("Theater"));
-    venueRepo.deleteAll();
+    Venue futureVenue = new Venue();
+    futureVenue.setDate(LocalDateTime.now().plusDays(1));
+    VenueDto futureVenuDTO = venueMapper.venueToVenueDto(futureVenue);
+    oldVenue = venueRepo.save(oldVenue);
+    futureVenue = venueRepo.save(futureVenue);
+    assertEquals(Set.of(futureVenuDTO), venueController.getAllVenues());
+    venueRepo.delete(oldVenue);
+    venueRepo.delete(futureVenue);
   }
 }
