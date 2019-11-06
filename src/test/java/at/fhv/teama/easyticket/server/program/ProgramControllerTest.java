ffa05a3830a343f655fd42cdbc717a3813d01602
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-11-06 14:30:14 +0100

    fix: fix add empty test to ProgramControllerTest to stop it from failing

--- src/test/java/at/fhv/teama/easyticket/server/program/ProgramControllerTest.java
+++ src/test/java/at/fhv/teama/easyticket/server/program/ProgramControllerTest.java
@@ -1,5 +1,6 @@
 package at.fhv.teama.easyticket.server.program;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,8 +12,10 @@ import javax.transaction.Transactional;
 @SpringBootTest
 @Transactional
 public class ProgramControllerTest {
-    @Autowired private ProgramRepository programRepository;
-    @Autowired private ProgramController progamController;
-    @Autowired private ProgramMapper programMapper;
+  @Autowired private ProgramRepository programRepository;
+  @Autowired private ProgramController progamController;
+  @Autowired private ProgramMapper programMapper;
 
+  @Test
+  public void test() {}
 }
