9b40709ff6f9f3b2966647f312c2a65ebe0554ac
Sunpip <dasbauchgefuehl@gmail.com>
2019-11-06 08:07:17 +0100

    Change test

--- src/test/java/at/fhv/teama/easyticket/server/venue/ticket/TicketServiceTest.java
+++ src/test/java/at/fhv/teama/easyticket/server/venue/ticket/TicketServiceTest.java
@@ -7,21 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static at.fhv.teama.easyticket.server.venue.ticket.TicketState.*;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TicketServiceTest {
     @Autowired private TicketService ticketService;
-    private TicketRepository ticketRepo;
 
 
     @Before
     public void setup() {
-
+        
     }
 
     @Test
-    public void buyTickets() {
-
+    public void sellTicket() {
+        Ticket t = new Ticket();
+        t.setState(SOLD);
+        assertTrue(ticketService.sellTicket(t));
+        t.setState(FREE);
+        assertTrue(ticketService.sellTicket(t));
+        t.setState(RESERVERD);
+        assertTrue(ticketService.sellTicket(t));
     }
-
 }
