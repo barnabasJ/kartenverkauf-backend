d8f3521c2f3349d5a6d97b4853fa83c1819953ea
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-01-01 18:10:00 +0100

    fix: start messaging in servlet initializer for use in war

--- src/main/java/at/fhv/teama/easyticket/server/ServletInitializer.java
+++ src/main/java/at/fhv/teama/easyticket/server/ServletInitializer.java
@@ -1,12 +1,20 @@
 package at.fhv.teama.easyticket.server;
 
+import at.fhv.teama.easyticket.server.messaging.MessagingController;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.net.URISyntaxException;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        try {
+            MessagingController.start_broker();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         return application.sources(EasyTicketServerApplication.class);
     }
 
